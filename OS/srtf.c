ce96463f8fc56f4525b0e2bec1cb7899cab94b7b
Praise <185355773+Ph0enix22@users.noreply.github.com>
2026-08-05 10:27:18 +0530

    Add SRTF scheduling algorithm in srtf.c
    Implement SRTF scheduling algorithm to calculate waiting and turnaround times.

---
+++ OS/srtf.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main(){
+    int p[20],bt[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
+    float avg_wt, avg_tat;
+    printf("Enter the number of Process: ");
+    scanf("%d",&n);
+    printf("Enter the burst time for each process: \n");
+    for(i=0;i<n;i++){
+        printf("P%d: ",i+1);
+        scanf("%d",&bt[i]);
+        p[i]=i+1;
+    }
+    for(i=0;i<n;i++){
+        pos=i;
+        for(j=i+1;j<n;j++){
+            if(bt[j]<bt[pos])
+                pos=j;
+        }
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+    wt[0]=0;
+    for(i=1;i<n;i++){
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+        total+=wt[i];
+    }
+    avg_wt=(float)total/n;
+    total=0;
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++){
+        tat[i]=bt[i]+wt[i];
+        total+=tat[i];
+        printf("\nP%d\t\t%d\t\t\t%d\t\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
+    }
+    avg_tat=(float)total/n;
+    printf("\n\nAverage Waiting Time = %.2f",avg_wt);
+    printf("\nAverage Turnaround Time = %.2f",avg_tat);
+    return 0;
+}
